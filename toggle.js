90a965f220bc81b46f90b3c25d74dfee06c0bc16
ruth tuttie <tuttieruth97@gmail.com>
2025-09-24 11:28:43 +0300

    added accent animations css

--- toggle.js
+++ toggle.js
@@ -3,12 +3,4 @@ const modes = document.querySelectorAll('.logo');
 const light = document.querySelector('#js-light');
 
 
-    modes.forEach(mode => {
-      mode.addEventListener('click', () => {
-        mode.classList.remove('hidden')
-        mode.classList.add('hidden');
-        
-      })
-    })
-
 
